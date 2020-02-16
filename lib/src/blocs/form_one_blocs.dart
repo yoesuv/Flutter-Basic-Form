6715f8db7aa219a6dc4ebc215a63edaf9d8ab84a
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2020-02-16 10:29:45 +0700

    :pencil: add function to check storage permission

--- lib/src/blocs/form_one_blocs.dart
+++ lib/src/blocs/form_one_blocs.dart
@@ -1,3 +1,10 @@
+import 'package:permission_handler/permission_handler.dart';
+
 class FormOneBloc {
+
+  Future<bool> checkStoragePermission() async{
+    final PermissionStatus check = await PermissionHandler().checkPermissionStatus(PermissionGroup.storage);
+    return check == PermissionStatus.granted;
+  }
 
 }
